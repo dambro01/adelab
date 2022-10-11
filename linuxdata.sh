365319a19314c97303073ed56a6c196f4be6ba3c
dambro01 <64372168+dambro01@users.noreply.github.com>
2022-10-11 11:37:58 -0400

    Update linuxdata.sh

--- linuxdata.sh
+++ linuxdata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir /data
-/usr/bin/printf "o\nn\np\n1\n\n\nt\n83\nw\n" |fdisk /dev/disk/azure/scsi1/lun0
+/usr/bin/printf "o\nn\np\n1\n\n\nt\n83\nw\n" | /usr/sbin/fdisk /dev/disk/azure/scsi1/lun0
 /usr/bin/echo "y" | /usr/sbin/mkfs.xfs -f /dev/disk/azure/scsi1/lun0-part1
 /usr/sbin/partprobe /dev/disk/azure/scsi1/lun0-part1
 UUID="$(/usr/sbin/blkid -s UUID -o value /dev/disk/azure/scsi1/lun0-part1)"
