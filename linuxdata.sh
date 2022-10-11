20066f98d0aea12ee22484bd8f6981053afd5ddc
dambro01 <64372168+dambro01@users.noreply.github.com>
2022-10-11 17:48:27 -0400

    Update linuxdata.sh

--- linuxdata.sh
+++ linuxdata.sh
@@ -2,6 +2,6 @@
 
 mkdir /data
 parted -a opt /dev/disk/azure/scsi1/lun0 mkpart primary ext4 0% 100%
-mkfs -t ext4 /dev/disk/azure/scsi1/lun0-part1
+mkfs.ext4 /dev/disk/azure/scsi1/lun0-part1
 echo "/dev/disk/azure/scsi1/lun0-part1 /data ext4 defaults,nofail 0 0" >>/etc/fstab
 mount -a
