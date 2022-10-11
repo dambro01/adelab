107abc73b1e15e3d63613cc1544fe24cc8518d2b
dambro01 <64372168+dambro01@users.noreply.github.com>
2022-10-11 10:42:52 -0400

    Update linuxdata.sh

--- linuxdata.sh
+++ linuxdata.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 mkdir /data
-parted /dev/disk/azure/scsi1/lun0 mklabel gpt
-parted -a opt /dev/disk/azure/scsi1/lun0 mkpart primary ext4 0% 100%
-mkfs.ext4 /dev/disk/azure/scsi1/lun0-part1
+parted /dev/disk/azure/scsi1/lun0 --script mklabel gpt mkpart xfspart xfs 0% 100%
+mkfs.xfs /dev/disk/azure/scsi1/lun0
+partprobe /dev/disk/azure/scsi1/lun0-part1
 partprobe /dev/disk/azure/scsi1/lun0-part1
 UUID="$(blkid -s UUID -o value /dev/disk/azure/scsi1/lun0-part1)"
 echo "UUID=$UUID /data ext4 defaults,nofail 0 0" >>/etc/fstab
