2fa2b7ffc4b32a8ac53c9286bfe9d33a0bbf7961
dambro01 <64372168+dambro01@users.noreply.github.com>
2022-10-11 19:56:59 -0400

    Update linuxdata.sh

--- linuxdata.sh
+++ linuxdata.sh
@@ -3,6 +3,12 @@
 mkdir /data
 parted -s /dev/disk/azure/scsi1/lun0 mklabel gpt
 parted -s /dev/disk/azure/scsi1/lun0 mkpart primary ext4 0% 100%
-mkfs.ext4 /dev/disk/azure/scsi1/lun0-part1
-echo "/dev/disk/azure/scsi1/lun0-part1 /data ext4 defaults,nofail 0 0" >>/etc/fstab
-mount -a
+mkfs.ext4 /dev/disk/azure/scsi1/lun0-part1 > /var/log/adelab.out 2>&1
+if [ echo $? -eq 0 ]; then 
+  echo "successful"" >> /var/log/adelab.out
+  echo "/dev/disk/azure/scsi1/lun0-part1 /data ext4 defaults,nofail 0 0" >>/etc/fstab
+  exit
+else 
+  echo "failed"" >> /var/log/adelab.out
+fi
+mount /dev/disk/azure/scsi1/lun0-part1 /data
