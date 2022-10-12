c64881976f136825833f4a7815ea6f739d775880
dambro01 <64372168+dambro01@users.noreply.github.com>
2022-10-11 20:22:49 -0400

    Update linuxdata.sh

--- linuxdata.sh
+++ linuxdata.sh
@@ -5,6 +5,6 @@ parted -s /dev/disk/azure/scsi1/lun0 mklabel gpt
 parted -s /dev/disk/azure/scsi1/lun0 mkpart primary ext4 0% 100%
 sleep 5
 mkfs.ext4 /dev/disk/azure/scsi1/lun0-part1
-sleep 30
+sleep 10
 echo "/dev/disk/azure/scsi1/lun0-part1 /data ext4 defaults,nofail 0 0" >>/etc/fstab
 mount /dev/disk/azure/scsi1/lun0-part1 /data
