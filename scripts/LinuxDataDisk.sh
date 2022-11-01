96a5f659029f9abe7a54b14000451c1e58dc45cb
dambro01 <64372168+dambro01@users.noreply.github.com>
2022-11-01 12:46:39 -0400

    Update LinuxDataDisk.sh

--- scripts/LinuxDataDisk.sh
+++ scripts/LinuxDataDisk.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 mkdir /data
-parted -s /dev/disk/azure/scsi1/lun0 mklabel gpt
-parted -s /dev/disk/azure/scsi1/lun0 mkpart primary 0% 100%
+parted /dev/disk/azure/scsi1/lun0 --script mklabel gpt mkpart xfspart xfs 0% 100%
+mkfs.xfs -f /dev/disk/azure/scsi1/lun0-part1
 sleep 5
-partprobe
-mkfs.xfs /dev/disk/azure/scsi1/lun0-part1
+mkfs.xfs -f /dev/disk/azure/scsi1/lun0-part1
+partprobe /dev/disk/azure/scsi1/lun0-part1
 sleep 10
 UUID="$(blkid -s UUID -o value /dev/disk/azure/scsi1/lun0-part1)" 
 echo "UUID=$UUID /data xfs defaults,nofail 0 0" >>/etc/fstab
-mount /dev/disk/azure/scsi1/lun0-part1 /data
+mount -a
 
