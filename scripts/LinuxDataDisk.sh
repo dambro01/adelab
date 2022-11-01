3ae647432de97a7afda4af5bae43879b6b37fe64
dambro01 <64372168+dambro01@users.noreply.github.com>
2022-11-01 10:09:36 -0400

    Update LinuxDataDisk.sh

--- scripts/LinuxDataDisk.sh
+++ scripts/LinuxDataDisk.sh
@@ -7,6 +7,6 @@ partprobe
 mkfs.xfs /dev/disk/azure/scsi1/lun0-part1
 sleep 10
 UUID="$(blkid -s UUID -o value /dev/disk/azure/scsi1/lun0-part1)" 
-echo "UUID=$UUID /data ext4 defaults,nofail 0 0" >>/etc/fstab
+echo "UUID=$UUID /data xfs defaults,nofail 0 0" >>/etc/fstab
 mount /dev/disk/azure/scsi1/lun0-part1 /data
 
