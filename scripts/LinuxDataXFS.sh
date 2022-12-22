6ebf4c0a13e1c11461e6644591f4d58ba290dc01
dambro01 <64372168+dambro01@users.noreply.github.com>
2022-12-21 23:49:43 -0500

    Add files via upload

---
+++ scripts/LinuxDataXFS.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir /data
+parted /dev/disk/azure/scsi1/lun0 --script mklabel gpt mkpart xfspart xfs 0% 100%
+sleep 5
+mkfs.xfs -f /dev/disk/azure/scsi1/lun0-part1
+partprobe /dev/disk/azure/scsi1/lun0-part1
+sleep 10
+UUID="$(blkid -s UUID -o value /dev/disk/azure/scsi1/lun0-part1)" 
+echo "UUID=$UUID /data xfs defaults,nofail 0 0" >>/etc/fstab
+mount -a
+
