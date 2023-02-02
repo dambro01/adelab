9987bb0833132a55d0aac4359128e07fb7222d06
dambro01 <64372168+dambro01@users.noreply.github.com>
2023-02-02 11:38:55 -0500

    Update Grub.sh

--- scripts/Grub.sh
+++ scripts/Grub.sh
@@ -2,7 +2,7 @@
 sleep 60
 log="/var/log/azure/Microsoft.Azure.Security.AzureDiskEncryptionForLinux/extension.log"
 
-if grep -q "exiting daemon" $log; then
+if grep "exiting daemon" $log; then
   mv /boot/grub/grub.cfg /tmp/grub.cfg
   reboot
 fi
