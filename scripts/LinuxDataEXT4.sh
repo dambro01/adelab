43e98ea1ed8e00b6a1ccc7de95ad2ec9c025834c
dambro01 <64372168+dambro01@users.noreply.github.com>
2023-03-10 10:05:41 -0500

    Update LinuxDataEXT4.sh

--- scripts/LinuxDataEXT4.sh
+++ scripts/LinuxDataEXT4.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 mkdir /data
-parted /dev/disk/azure/scsi1/lun0 --script mklabel gpt
-sleep 5
+parted /dev/disk/azure/scsi1/lun0 mklabel gpt
+sleep 10
 parted -a opt /dev/disk/azure/scsi1/lun0 mkpart primary ext4 0% 100%
-mkfs.ext4 /dev/disk/azure/scsi1/lun0-part1
 sleep 10
-partprobe /dev/disk/azure/scsi1/lun0-part1
+mkfs -t ext4 /dev/disk/azure/scsi1/lun0-part1
 sleep 10
-UUID="$(blkid -s UUID -o value /dev/disk/azure/scsi1/lun0-part1)"
-echo "UUID=$UUID /data ext4 defaults,nofail 0 0" >>/etc/fstab
-mount -a
+mount /dev/disk/azure/scsi1/lun0-part1 /data
+echo "/dev/disk/azure/scsi1/lun0-part1 /data ext4 defaults,nofail 0 2" >> /etc/fstab
