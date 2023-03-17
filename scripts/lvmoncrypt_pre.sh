a71f7d252c156fa7e181517b118da8f2143741de
dambro01 <64372168+dambro01@users.noreply.github.com>
2023-03-17 16:30:38 -0400

    Update lvmoncrypt_pre.sh

--- scripts/lvmoncrypt_pre.sh
+++ scripts/lvmoncrypt_pre.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-mkdir /tempdata0
-mkdir /tempdata1
+log="/var/log/azure/Microsoft.Azure.Security.AzureDiskEncryptionForLinux/extension.log"
+log_file="/var/log/azure/run.log"
 
-mkfs.ext4 -F /dev/disk/azure/scsi1/lun0
-sleep 5 
-mkfs.ext4 -F /dev/disk/azure/scsi1/lun1
-sleep 5
+now=$(date '+%m%d%y')
+echo "$now: Running the script" >> $log_file
 
-diskuuid0="$(blkid -s UUID -o value /dev/disk/azure/scsi1/lun0)"
-diskuuid1="$(blkid -s UUID -o value /dev/disk/azure/scsi1/lun1)"
+if grep "exiting daemon" $log; then
+  mkdir /tempdata0
+  mkdir /tempdata1
 
-echo "UUID=$diskuuid0 /tempdata0 ext4 defaults,nofail 0 0" >> /etc/fstab
-echo "UUID=$diskuuid1 /tempdata1 ext4 defaults,nofail 0 0" >> /etc/fstab
+  mkfs.ext4 -F /dev/disk/azure/scsi1/lun0
+  sleep 5 
+  mkfs.ext4 -F /dev/disk/azure/scsi1/lun1
+  sleep 5
 
-mount -a
+  diskuuid0="$(blkid -s UUID -o value /dev/disk/azure/scsi1/lun0)"
+  diskuuid1="$(blkid -s UUID -o value /dev/disk/azure/scsi1/lun1)"
 
+  echo "UUID=$diskuuid0 /tempdata0 ext4 defaults,nofail 0 0" >> /etc/fstab
+  echo "UUID=$diskuuid1 /tempdata1 ext4 defaults,nofail 0 0" >> /etc/fstab
 
+  mount -a
+else
+  echo "$now: 'exiting daemon' not found in $log" >> $log_file
+fi
