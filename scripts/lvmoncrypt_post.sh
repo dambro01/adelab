d09c031735ab512dfa0cb638373bfbc646f0bfea
dambro01 <64372168+dambro01@users.noreply.github.com>
2023-03-17 12:19:34 -0400

    Create lvmoncrypt_post.sh

---
+++ scripts/lvmoncrypt_post.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+umount -lf /tempdata0
+umount -lf /tempdata1
+
+uuiddata="$(grep tempdata0 /etc/fstab | awk -F/ '{print $4}')"
+uuidapp="$(grep tempdata1 /etc/fstab | awk -F/ '{print $4}')"
+
+sed '/tempdata/d' /etc/fstab > /etc/fstab.bk
+mv /etc/fstab.bk /etc/fstab
+
+echo "y" | pvcreate /dev/mapper/$uuiddata
+echo "y" | pvcreate /dev/mapper/$uuidapp
+
+vgcreate vgdata /dev/mapper/$uuiddata
+vgcreate vgapp /dev/mapper/$uuidapp
+
+lvcreate -L 3G -n lvdata vgdata
+lvcreate -L 3G -n lvapp vgapp
+
+echo "yes" | mkfs.ext4 /dev/vgdata/lvdata
+echo "yes" | mkfs.ext4 /dev/vgapp/lvapp
+
+mkdir /data
+mkdir /app
+
+echo "/dev/mapper/vgdata-lvdata /data ext4 defaults,nofail 0 0" >> /etc/fstab
+echo "/dev/mapper/vgapp-lvapp /app ext4 defaults,nofail 0 0" >> /etc/fstab
+
+mount -a
