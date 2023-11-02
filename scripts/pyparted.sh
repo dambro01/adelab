3c4a695d51e95183a584de8bb6a6e80d04d0ef84
dambro01 <64372168+dambro01@users.noreply.github.com>
2023-11-01 21:27:44 -0400

    Update pyparted.sh

--- scripts/pyparted.sh
+++ scripts/pyparted.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-if apt remove python3-parted -y; then
-    echo "The package python3-parted was successfully removed."
+if dpkg -s python3-parted >/dev/null 2>&1; then
+    echo "python3-parted package is installed. Removing it now..." >> /var/tmp/cse
+    apt remove python3-parted -y >> /var/tmp/cse 2>&1
+    echo "python3-parted package has been successfully removed." >> /var/tmp/cse
 else
-    echo "There was an error removing the package python3-parted."
+    echo "There was an error removing the package python3-parted." >> /var/tmp/cse
     exit 1
 fi
