8d0400db3c93ad0cea326a0516110f469086f030
dambro01 <64372168+dambro01@users.noreply.github.com>
2023-11-16 12:38:18 -0500

    Update and rename disk_config.sh to sap_disk_config.sh

--- scripts/sap_disk_config.sh
+++ scripts/sap_disk_config.sh
@@ -74,3 +74,24 @@ sudo mount -a | tee -i -a "$logFile"
 # Report file system to log
 echo "<<< Output of df -h >>>" >> $logFile
 df -h | tee -i -a "$logFile"
+
+echo "<<< install socat >>>" >> $logFile
+sudo zypper -n install socat | tee -i -a "$logFile"
+
+echo "<<< install resource-agents >>>" >> $logFile
+sudo zypper -n install resource-agents | tee -i -a "$logFile"
+
+echo "<<< install fence-agents >>>" >> $logFile
+sudo zypper -n install fence-agents | tee -i -a "$logFile"
+
+echo "<<< install python3-azure-mgmt-compute >>>" >> $logFile
+sudo zypper -n install python3-azure-mgmt-compute | tee -i -a "$logFile"
+
+echo "<<< install python3-azure-identity >>>" >> $logFile
+sudo zypper -n install python3-azure-identity | tee -i -a "$logFile"
+
+echo "<<< perform zypper update >>>" >> $logFile
+sudo zypper -n update | tee -i -a "$logFile"
+
+echo "<<< reboot VM >>>" >> $logFile
+sudo shutdown -r
